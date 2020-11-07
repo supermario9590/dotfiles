37465709ecc67dc51a096a6f7533b7b908f5ea2e
Super Mario <anishreddyvundela@gmail.com>
2020-11-07 17:26:57 +0000

    Minor edits

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -413,17 +413,6 @@
       (org-babel-tangle)))
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'av/org-babel-tangle-config)))
 
-;; EAF
-
-(use-package eaf
-  :load-path "/usr/share/emacs-lisp/site-lisp/eaf"; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
-  :custom
-  (eaf-find-alternate-file-in-dired t)
-  :config
-  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
-  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
-  (eaf-bind-key take_photo "p" eaf-camera-keybinding))
-
 ;; HASKELL
 
 (use-package haskell-mode)
